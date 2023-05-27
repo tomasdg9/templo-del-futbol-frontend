b538f82ab3bbc03193a4be731ebea7bd20abd569
Nicolás Berti <71272720+nicolasberti@users.noreply.github.com>
2023-05-27 11:42:11 -0300

    Fix API

--- src/componentes/entidades/ProductoDetalle.js
+++ src/componentes/entidades/ProductoDetalle.js
@@ -10,28 +10,14 @@ function ProductoDetalle() {
 
   const [producto, setProducto] = useState(null);
   const [categoria, setCategoria] = useState(null);
-  const valido = Number.isInteger(Number(id)) && Number(id) >= 0;
+  const [valido, setValido] = useState(true);
   const { agregarProducto } = useContext(CarritoContexto);
 
   useEffect(() => {
-    if (!valido) {
-      return;
-    }
-
+    setValido(Number.isInteger(Number(id)) && Number(id) >= 0);
     obtenerProducto();
-    //obtenerCategoria();
-  }, [valido]);
-
-  const obtenerProducto = () => {
-    const URL = "https://de-giusti-berti-laravel-tomasdg9.vercel.app/rest/productos/" + id;
-    fetch(URL)
-      .then(respuesta => respuesta.json())
-      .then(resultado => {
-        setProducto(resultado);
-      })
-      .catch(error => console.log(error));
-  }
-
+  }, []);
+  
   if (!valido) {
     return <Navigate to="/not-found" />;
   }
@@ -40,76 +26,59 @@ function ProductoDetalle() {
     return <div>Cargando...</div>;
   }
 
-  const { nombre, imagen, descripcion, precio, stock, estado, categoria_id, nombre_categoria } = producto;
-
-  const handleClickCard = () => {
+  function handleClickCard() {
     console.log('se hizo click en la card. Deberia ir al link del producto');
   };
 
-  const agregarACarrito = () => {
-    agregarProducto(id);
-    console.log('Se hizo clic en el botón Comprar');
-  };
-
-  
-
- const obtenerCategoria = () => {
-    if (!categoria_id) {
-      return;
-    }
-    
-    const URL = "https://de-giusti-berti-laravel-tomasdg9.vercel.app/rest/productos/categoria/" + categoria_id;
+  function obtenerProducto() {
+    const URL = "https://de-giusti-berti-laravel-tomasdg9.vercel.app/rest/productos/" + id;
     fetch(URL)
-      .then(respuesta => respuesta.json())
-      .then(resultados => {
-        setCategoria(resultados); // Guardar los resultados directamente
-        
-        // Agrupar los productos en un arreglo
-        const productos = resultados.map(resultado => resultado.producto);
-        console.log(productos);
-        
-        // Realizar otras operaciones con la colección de productos agrupados
-        // ...
+      .then(respuesta => {
+        if (respuesta.status === 200) return respuesta.json();
+        else setValido(false);
+      })
+      .then(resultado => {
+        setProducto(resultado);
       })
       .catch(error => console.log(error));
   }
-
+  
 return (      
-      <div class="container-lg">
+      <div className="container-lg">
         <Card className="cardDetalleProd">
           <div className='productoDetalle'>
            
-          <div class='contenedorTitulo'><h1>{nombre}</h1></div>
-            <div class="row">
-              <div class="col text-center">
-                <img src={imagen} ></img>{/* Obtener la imagen por id */}
-                <div class="my-4"></div>
+          <div className='contenedorTitulo'><h1>{producto.nombre}</h1></div>
+            <div className="row">
+              <div className="col text-center">
+                <img src={producto.imagen} ></img>{/* Obtener la imagen por id */}
+                <div className="my-4"></div>
               </div>
-              <div class="col">
+              <div className="col">
               <Card className='cardProdDescripcion'>
-                <div class="row">
-                  <p class='categoriaProducto'>{nombre_categoria}</p>{/* Esta seria la categoria */}
+                <div className="row">
+                  <p className='categoriaProducto'>{producto.nombre_categoria}</p>{/* Esta seria la categoria */}
                   </div>
-                <div class="descripcion">
-                    <p>{descripcion}</p>
-                    <p>{estado}</p>
-                    <p>Stock disponible: {stock}</p> {/** si no hay stock disponible hay que ver que se podria hacer, se podria mostrar el producto sin la opcion comprar */}
-                    <p class='precio'>${precio}</p>
+                <div className="descripcion">
+                    <p>{producto.descripcion}</p>
+                    <p>{producto.estado}</p>
+                    <p>Stock disponible: {producto.stock}</p> {/** si no hay stock disponible hay que ver que se podria hacer, se podria mostrar el producto sin la opcion comprar */}
+                    <p className='precio'>${producto.precio}</p>
                 </div>
                 <br/>
                     </Card>
               </div>
 
-              <div class='d-flex justify-content-center align-items-center'><BotonComprar class='botonProducto' onClick={() => agregarProducto(id)}></BotonComprar></div>
+              <div className='d-flex justify-content-center align-items-center'><BotonComprar className='botonProducto' onClick={() => agregarProducto(id)}></BotonComprar></div>
             </div>
           </div>
           
         </Card>
       <br/>
-      <div class='d-flex justify-content-center align-items-center'>
+      <div className='d-flex justify-content-center align-items-center'>
         <Card className='cardDetalleProd'>
           
-        <div class="row">
+        <div className="row">
            
          
         <div className="col-md-3 col-sm-6 mb-5">
