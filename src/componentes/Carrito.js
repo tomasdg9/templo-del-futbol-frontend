4d152d2cd2c1764a41bf3f2a2937a41965b1b8b2
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-06-22 17:07:29 -0300

    conflictos merge

--- src/componentes/Carrito.js
+++ src/componentes/Carrito.js
@@ -295,54 +295,6 @@ const Carrito = (props) => {
     }
   };
 
-  const initialization = {
-    amount: 100,
-    preferenceId: preferenceID,
-  };
-  const customization = {
-    paymentMethods: {
-      ticket: "all",
-      creditCard: "all",
-      debitCard: "all",
-      mercadoPago: "all",
-    },
-  };
-  const onSubmit = async (
-    { selectedPaymentMethod, formData }
-  ) => {
-    // callback llamado al hacer clic en el botón enviar datos
-    return new Promise((resolve, reject) => {
-      fetch("/process_payment", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          // recibir el resultado del pago
-          resolve();
-        })
-        .catch((error) => {
-          // manejar la respuesta de error al intentar crear el pago
-          console.log("Error en el pago");
-          reject();
-        });
-    });
-  };
-  const onError = async (error) => {
-    // callback llamado para todos los casos de error de Brick
-    console.log(error);
-  };
-  const onReady = async () => {
-    /*
-      Callback llamado cuando el Brick está listo.
-      Aquí puede ocultar cargamentos de su sitio, por ejemplo.
-    */
-  };
- 
-
   useEffect(() => {
     obtenerProductos();
     initMercadoPago('TEST-9d1c09e5-7670-4cbb-97cf-ede37dc650e7', {locale: 'es-AR'});
